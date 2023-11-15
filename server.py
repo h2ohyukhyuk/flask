ff6d539b1474ab80233182a60168ee44440585d1
h2ohyukhyuk <h2ohyukhyuk@naver.com>
2023-11-15 09:46:38 +0900

    simple html

--- server.py
+++ server.py
@@ -1,9 +1,47 @@
 from flask import Flask
+import random
 
 app = Flask(__name__)
 
+topics = [
+    {'id': 1, 'title': 'html', 'body': 'html is ...'},
+    {'id': 2, 'title': 'css', 'body': 'css is ...'},
+    {'id': 3, 'title': 'javascript', 'body': 'javascript is ...'}
+]
+
+def template(contents, content):
+    return f'''
+    <!doctype html>
+    <body>
+        <h1><a href="/">WEB</a></h1>
+        <ol>
+            {contents}
+        </ol>
+        {content}
+    </body>
+    '''
+
+def getContents():
+    liTags = ''
+    for topic in topics:
+        liTags += f"<li> <a href='/read/{topic['id']}'> {topic['title']} </a> </li>"
+    return liTags
+
+
 @app.route('/')
 def index():
-    return 'hello world'
+    return template(getContents(), '<h2>Welcome</h2>')
+
+@app.route('/read/<int:id>/')
+def read(id):
+    title, body = '', ''
+    for topic in topics:
+        if id == topic['id']:
+            title = topic['title']
+            body = topic['body']
+            break
+
+
+    return template(getContents(), f'<h2>{title}</h2>{body}')
 
 app.run(port=5001, debug=True)
